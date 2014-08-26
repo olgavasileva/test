219d2af992610b6715272117c2d29df98a09f780
kroxy <kreiter.tibor@gmail.com>
2014-08-26 17:32:01 +0100

    delete_tirth_row_from_feed

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,7 @@
 class QuestionsController < ApplicationController
 
   def index
-    per_page = 9
+    per_page = 6
     @questions = policy_scope(Question).paginate(page: params[:page], per_page:per_page)
 
     if user_signed_in? && @questions.count < per_page * params[:page].to_i + per_page + 1
