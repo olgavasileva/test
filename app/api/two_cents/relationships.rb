a9609f880dd9541d03e2bddb876456c538e06312
Artur Ostrega <skoofoo@gmail.com>
2014-08-29 21:03:59 -0500

    relationships/followers API

--- app/api/two_cents/relationships.rb
+++ app/api/two_cents/relationships.rb
@@ -3,7 +3,34 @@ class TwoCents::Relationships < Grape::API
 
     # followers
     desc "List users following us"
-    get 'followers'
+    params do
+      requires :auth_token, type: String, desc: "Obtain this from the instance's API."
+
+      optional :user_id, type: Integer, desc: "User ID. Defaults to logged in user's ID."
+      optional :page, type: Integer, desc: "Page number, minimum 1. If left blank, responds with all questions."
+      optional :per_page, type: Integer, default: 15, desc: "Number of questions per page."
+    end
+    get 'followers' do
+      user_id = params[:user_id]
+      user = user_id.present? ? User.find(user_id) : current_user
+
+      followers = user.followers
+
+      if params[:page]
+        followers = followers.paginate(page: params[:page],
+                                       per_page: params[:per_page])
+      end
+
+      followers.map do |f|
+        {
+          id: f.id,
+          username: f.username,
+          email: f.email,
+          name: f.name,
+          group_ids: f.groups.map(&:id)
+        }
+      end
+    end
 
     # following
     desc "List users being followed"
