c008fdaebffdd5f07764d02a8b066540d2cc52a4
Artur Ostrega <skoofoo@gmail.com>
2014-08-29 21:12:08 -0500

    relationships/following API

--- app/api/two_cents/relationships.rb
+++ app/api/two_cents/relationships.rb
@@ -7,8 +7,8 @@ class TwoCents::Relationships < Grape::API
       requires :auth_token, type: String, desc: "Obtain this from the instance's API."
 
       optional :user_id, type: Integer, desc: "User ID. Defaults to logged in user's ID."
-      optional :page, type: Integer, desc: "Page number, minimum 1. If left blank, responds with all questions."
-      optional :per_page, type: Integer, default: 15, desc: "Number of questions per page."
+      optional :page, type: Integer, desc: "Page number, minimum 1. If left blank, responds with all."
+      optional :per_page, type: Integer, default: 15, desc: "Number of followers per page."
     end
     get 'followers' do
       user_id = params[:user_id]
@@ -34,7 +34,34 @@ class TwoCents::Relationships < Grape::API
 
     # following
     desc "List users being followed"
-    get 'following'
+    params do
+      requires :auth_token, type: String, desc: "Obtain this from the instance's API."
+
+      optional :user_id, type: Integer, desc: "User ID. Defaults to logged in user's ID."
+      optional :page, type: Integer, desc: "Page number, minimum 1. If left blank, responds with all."
+      optional :per_page, type: Integer, default: 15, desc: "Number of users per page."
+    end
+    get 'following' do
+      user_id = params[:user_id]
+      user = user_id.present? ? User.find(user_id) : current_user
+
+      leaders = user.leaders
+
+      if params[:page]
+        leaders = leaders.paginate(page: params[:page],
+                                       per_page: params[:per_page])
+      end
+
+      leaders.map do |l|
+        {
+          id: l.id,
+          username: l.username,
+          email: l.email,
+          name: l.name,
+          group_ids: l.groups.map(&:id)
+        }
+      end
+    end
 
     # follow
     desc "Follow a user"
