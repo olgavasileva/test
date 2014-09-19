d92a7b2bab3af3ef9aad87312a2ca487d5f6b270
Artur Ostrega <skoofoo@gmail.com>
2014-09-18 19:38:19 -0500

    added messages data fields

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140918011046) do
+ActiveRecord::Schema.define(version: 20140919000655) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -343,6 +343,10 @@ ActiveRecord::Schema.define(version: 20140918011046) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "response_count", default: 0
+    t.integer  "comment_count",  default: 0
+    t.integer  "share_count",    default: 0
+    t.datetime "completed_at"
   end
 
   add_index "messages", ["user_id"], name: "index_messages_on_user_id", using: :btree
