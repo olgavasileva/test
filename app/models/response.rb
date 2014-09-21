a1ac11ce6c670f1ccf3f20d9cfb6b225cab484e7
borys216307 <borys216307@outlook.com>
2014-09-21 18:52:09 +0200

    some errors solved

--- app/models/response.rb
+++ app/models/response.rb
@@ -39,6 +39,7 @@ class Response < ActiveRecord::Base
       message.comment_count = self.question.comment_count
       message.share_count = self.question.share_count
 
+      message.read_at = nil
       message.completed_at = Time.now() if self.question.targeting?
 
       message.save
