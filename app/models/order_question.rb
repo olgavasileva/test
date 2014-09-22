e0be392fe80dd71f0a0f834afee062ecedb08d20
Artur Ostrega <skoofoo@gmail.com>
2014-09-21 23:03:08 -0500

    some question types' image generated from choices

--- app/models/order_question.rb
+++ app/models/order_question.rb
@@ -1,4 +1,7 @@
 class OrderQuestion < ChoiceQuestion
+  include BackgroundImageFromChoices
+
+  belongs_to :background_image
   has_many :choices, class_name:"OrderChoice", foreign_key: :question_id, dependent: :destroy, inverse_of: :question
   has_many :responses, class_name:"OrderResponse", foreign_key: :question_id
 
