253090e338cdd74d7ea8841fda6ae1ab9b8e90ba
Artur Ostrega <skoofoo@gmail.com>
2014-09-22 20:30:41 -0500

    removed environment.rb AWS config overwrites

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,5 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
-ENV['AWS_ACCESS_KEY'] = '12334124'
-ENV['AWS_SECRET_ACCESS_KEY'] = '2131221'
-ENV['AWS_REGION'] = 'adsfas'
 # Initialize the Rails application.
 LinkchatApp::Application.initialize!
