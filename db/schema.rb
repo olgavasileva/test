1168dbe4e5bdbbb1626ef6c42fa9dd7cae0928f8
borys216307 <borys216307@outlook.com>
2014-09-28 23:35:32 +0200

    update

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20140924221528) do
-=======
+
 ActiveRecord::Schema.define(version: 20140928211329) do
->>>>>>> labs_message_api
+
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
