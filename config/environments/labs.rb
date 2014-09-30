0674112d34d4ba9735a2687414b90ad26614f081
borys216307 <borys216307@outlook.com>
2014-09-30 19:04:48 +0200

    register api updated

--- config/environments/labs.rb
+++ config/environments/labs.rb
@@ -1,6 +1,9 @@
 # Based on production defaults
 require Rails.root.join("config/environments/production")
-
+ENV['AWS_ACCESS_KEY'] = 'ASD'
+ENV['AWS_SECRET_ACCESS_KEY'] = 'ASD'
+ENV['AWS_REGION'] = 'ASD'
+ENV['AWS_BUCKET'] = 'ASD'
 
 LinkchatApp::Application.configure do
 
--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -7,9 +7,9 @@ CarrierWave.configure do |config|
 
     config.fog_credentials = {
       provider:              'AWS',                                 # required
-      aws_access_key_id:     ENV['AWS_ACCESS_KEY'],                 # required
-      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],          # required
-      region:                ENV['AWS_REGION'],                     # optional, defaults to 'us-east-1'
+      aws_access_key_id:     'asdfasdf',                 # required
+      aws_secret_access_key: 'asdasdfasdf',          # required
+      region:                'asdfasdds',                     # optional, defaults to 'us-east-1'
       # host:                  's3.example.com',                    # optional, defaults to nil
       # endpoint:              'https://s3.example.com:8080'        # optional, defaults to nil
     }
