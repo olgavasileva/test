e420fecdb15b5f0a248deb941f738e2862b1c6df
borys216307 <borys216307@outlook.com>
2014-10-02 21:35:32 +0200

    duplicate messages removed

--- app/models/feed_item.rb
+++ app/models/feed_item.rb
@@ -2,8 +2,9 @@ class FeedItem < ActiveRecord::Base
   belongs_to :user
   belongs_to :question
 
-  after_create :add_and_push_message
+  # after_create :add_and_push_message
 
+  protected
   def add_and_push_message
 
 
