de5aacbf06329c59c353e6db47a54b02d70df83c
Artur Ostrega <artur.mariusz.ostrega@gmail.com>
2014-10-17 18:23:46 -0500

    feed algorithm targeted questions

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,8 @@
 class Group < ActiveRecord::Base
   belongs_to :user
-  has_many :members, class_name:"GroupMember", dependent: :destroy
-  has_many :member_users, class_name:"User", through: :members, source: :user
+  has_many :members, class_name: "GroupMember", dependent: :destroy
+  has_many :member_users, through: :members, source: :user
+  has_many :groups_targets
 
   validates :name, uniqueness: { scope: :user_id,
                                  message: "already used by this user" }
