1c2774616f5810209780888f013b7cf94a2cd0f9
Artur Ostrega <artur.mariusz.ostrega@gmail.com>
2014-10-21 17:32:35 -0500

    fixed order choice image uploader

--- app/uploaders/order_choice_image_uploader.rb
+++ app/uploaders/order_choice_image_uploader.rb
@@ -2,7 +2,7 @@
 
 class OrderChoiceImageUploader < RetinaImageUploader
 
-  responsive_version :web, [320,80]
-  responsive_version :device, [320,80]
+  responsive_version :web, [320,320]
+  responsive_version :device, [320,320]
 
 end
