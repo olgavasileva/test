a0775e24f29412589ca6794f072a767a730cc1c3
Artur Ostrega <artur.mariusz.ostrega@gmail.com>
2014-10-21 16:06:32 -0500

    anonymous question posting

--- app/controllers/targets_controller.rb
+++ app/controllers/targets_controller.rb
@@ -8,7 +8,7 @@ class TargetsController < ApplicationController
   end
 
   def create
-    question = Question.find params[:question_id]
+    question = Question.find params.fetch(:question_id)
     target = question.build_target target_params
     authorize question.target
 
@@ -16,6 +16,8 @@ class TargetsController < ApplicationController
     target_count = question.apply_target! target  # TODO - do this on a background resque queue or delayed job - it will take time when there are lots of users
     reports = target.public? ? ["the public feed"] : []
     reports << view_context.pluralize( target_count, "direct feed") if target_count > 0
+
+    question.update_attributes(anonymous: params.fetch(:anonymous))
 
     flash[:alert] = "Question added to #{ reports.join " and " }."
     redirect_to results_question_path(question)
