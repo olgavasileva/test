fe2dd64abd5caa0985403a64c223db04983510c3
Troy Anderson (laptop) <troy@tlainvestments.com>
2014-10-22 09:43:25 -0700

    Fixed bars on prioritizer

--- app/models/concerns/background_image_from_choices.rb
+++ app/models/concerns/background_image_from_choices.rb
@@ -13,10 +13,16 @@ module BackgroundImageFromChoices
 
       return if choice_images.empty?
 
+      tile, geometry = if self.kind_of?(OrderQuestion)
+        ['1x4','160x80+0+0']
+      else
+        ['2x2','160x160+0+0']
+      end
+
       montage = Magick::ImageList.new(*choice_images).montage do |m|
-        m.tile = '2x2'
+        m.tile = tile
         m.background_color = '#a4a5a9'
-        m.geometry = '160x160+0+0'
+        m.geometry = geometry
       end
 
       file = Tempfile.new(['question_image', '.jpg'])
