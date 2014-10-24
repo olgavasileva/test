a1450a7698d6e6992b73527f59737db2e974a6b5
Troy Anderson (laptop) <troy@tlainvestments.com>
2014-10-24 15:09:00 -0700

    Added placeholder images for the studio responses.

--- app/uploaders/scene_image_uploader.rb
+++ app/uploaders/scene_image_uploader.rb
@@ -3,6 +3,10 @@ class SceneImageUploader < UploaderBase
 
   include CarrierWave::RMagick
 
+  def default_url
+    ActionController::Base.helpers.asset_path "fallback/sample_studio.jpg"
+  end
+
   version :thumb do
     process :resize_to_fill => [400,275]
   end
