5699361b080448d0671227761427eceadc9c228d
borys216307 <borys216307@outlook.com>
2014-10-27 16:58:12 +0100

    direct targeting problem

--- app/models/target.rb
+++ app/models/target.rb
@@ -27,6 +27,7 @@ class Target < ActiveRecord::Base
         user.followers.each do |follower|
           if follower.wants_question? question
             follower.feed_questions << question
+            add_and_push_message follower, question
             target_count += 1
           end
         end
@@ -34,6 +35,7 @@ class Target < ActiveRecord::Base
         followers.each do |follower|
           if follower.leaders.include?(user) && follower.wants_question?(question)
             follower.feed_questions << question
+            add_and_push_message follower, question
             target_count += 1
           end
         end
@@ -43,6 +45,7 @@ class Target < ActiveRecord::Base
         user.group_members.each do |member|
           if member.wants_question? question
             member.feed_questions << question
+            add_and_push_message member, question
             target_count += 1
           end
         end
@@ -52,6 +55,7 @@ class Target < ActiveRecord::Base
             group.member_users.each do |member_user|
               if member_user.wants_question? question
                 member_user.feed_questions << question
+                add_and_push_message member_user, question
                 target_count += 1
               end
             end
@@ -69,5 +73,61 @@ class Target < ActiveRecord::Base
       self.all_users = false if all_users.nil?
       self.all_followers = false if all_followers.nil?
       self.all_groups = false if all_groups.nil?
+    end
+
+
+    def add_and_push_message(targeted_user, question)
+
+
+      message = QuestionTargeted.new
+
+      message.user_id = targeted_user.user_id
+      message.question_id = question.question_id
+
+      user_name = question.anonymous? ? "Someone" : question.user.username
+      message.body = "#{user_name} has a question for you"
+
+      message.save
+
+      targeted_user.instances.each do |instance|
+        next unless instance.push_token.present?
+
+        instance.push alert:'Hello iPhone!', badge:0, sound:true, other: {:type => message.type,
+                                                                          :created_at => message.created_at,
+                                                                          :read_at => message.read_at,
+                                                                          :question_id => message.question_id,
+                                                                          :body => message.body }
+      end
+
+      # if !QuestionTargeted.exists?(:user_id => self.user_id)
+      #   message = QuestionTargeted.new
+      # else
+      #   message = QuestionTargeted.find_by_user_id(self.user_id)
+      # end
+      #
+      #
+      # message.user_id = self.question.user_id
+      # message.question_id = self.question_id
+      #
+      # question = self.question
+      # user_name = question.anonymous? ? "Someone" : question.user.username
+      # message.body = "#{user_name} has a question for you"
+      #
+      # message.save
+      #
+      #
+      #
+      # self.user.instances.each do |instance|
+      #   next unless instance.push_token.present?
+      #
+      #   APNS.send_notification(instance.push_token, :alert => 'Hello iPhone!', :badge => 0, :sound => 'default',
+      #                          :other => {:type => message.type,
+      #                                     :created_at => message.created_at,
+      #                                     :read_at => message.read_at,
+      #                                     :question_id => message.question_id,
+      #                                     :body => message.body
+      #                          })
+      # end
+
     end
 end
