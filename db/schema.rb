700bbb4cb29b79782b0c0b70432e3fe8a74234ba
Artur Ostrega <artur.mariusz.ostrega@gmail.com>
2014-11-06 18:39:13 -0600

    User#feed_page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141031023656) do
+ActiveRecord::Schema.define(version: 20141106231819) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -837,6 +837,7 @@ ActiveRecord::Schema.define(version: 20141031023656) do
     t.string   "gender"
     t.string   "company_name"
     t.integer  "user_avatar_id"
+    t.integer  "feed_page",              default: 0
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
