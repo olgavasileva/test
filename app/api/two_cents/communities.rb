6ef2129088191219bdbc8f8a6a1ff4ed19251af5
Artur Ostrega <artur.mariusz.ostrega@gmail.com>
2014-11-21 16:25:56 -0600

    communities/summon_multiple exits

--- app/api/two_cents/communities.rb
+++ app/api/two_cents/communities.rb
@@ -202,7 +202,10 @@ class TwoCents::Communities < Grape::API
         message_setting = Setting.find_by_key('share_community_public')
       end
       url = Rails.application.routes.url_helpers.user_url(current_user, tab: 'communities')
-      message_text = "#{message_setting.value} #{url}"
+      message_text = message_setting.value
+      message_text.gsub!('%name%', community.name)
+      message_text.gsub!('%password%', community.password)
+      message_text = "#{message_text} #{url}"
 
       params[:email_addresses].each do |email_address|
         send_email(email_address, message_text)
