817cb71dd4047ca378bfd78dd71701520bc99d9a
Troy Anderson (laptop) <troy@tlainvestments.com>
2014-12-30 23:40:47 -0700

    Added show_unit.js for staging and production.

---
+++ public/staging/show_unit.js
@@ -0,0 +1,5 @@
+var iframe = document.createElement('iframe');
+iframe.width = statisfy_unit_width;
+iframe.height = statisfy_unit_height;
+iframe.src = "http://staging.statisfy.co/unit/" + statisfy_unit;
+document.body.appendChild(iframe);
