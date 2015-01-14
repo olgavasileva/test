1635d8512f2e9cc174111b4906e63d5a3f8c3b0e
Troy Anderson (laptop) <troy@tlainvestments.com>
2015-01-14 16:52:12 -0700

    Added cascade delete on comments.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,9 +2,9 @@ class Comment < ActiveRecord::Base
   belongs_to :user, class_name: "Respondent"
   belongs_to :commentable, :polymorphic => true
 
-  has_many :likes, class_name: 'LikedComment'
+  has_many :likes, class_name: 'LikedComment', depenent: :destroy
   has_many :likers, through: :likes, source: :user
-  has_many :comments, as: :commentable
+  has_many :comments, as: :commentable, depenent: :destroy
 
   validates :user, presence: true
   validates :body, presence: true
