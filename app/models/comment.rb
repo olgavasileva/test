66eb3ee326e836f918cc9b546f55f9bb9c307b7b
Troy Anderson (laptop) <troy@tlainvestments.com>
2015-01-14 18:43:01 -0700

    Fixed syntax error just introduced into comments.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,9 +2,9 @@ class Comment < ActiveRecord::Base
   belongs_to :user, class_name: "Respondent"
   belongs_to :commentable, :polymorphic => true
 
-  has_many :likes, class_name: 'LikedComment', depenent: :destroy
+  has_many :likes, class_name: 'LikedComment', dependent: :destroy
   has_many :likers, through: :likes, source: :user
-  has_many :comments, as: :commentable, depenent: :destroy
+  has_many :comments, as: :commentable, dependent: :destroy
 
   validates :user, presence: true
   validates :body, presence: true
