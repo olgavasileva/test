61474ede8f8edc0b45b9deaab5fdc50a75c36f57
Troy Anderson (laptop) <troy@tlainvestments.com>
2015-01-28 11:01:39 -0700

    Added "pro" to user profile so that the consuming app knows if the user is a "pro" and can present a login to the enterprise pages.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -6,6 +6,12 @@ FactoryGirl.define do
     password "testing123"
     after(:build) {|user| user.password_confirmation ||= user.password}
     birthdate Date.current-20.years
+
+    trait :pro do
+      after :create do |u|
+        u.add_role :pro
+      end
+    end
   end
 
   factory :anonymous do
