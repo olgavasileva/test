a0f3b241d1c7188e3a4b79b4c2a69ef3a14231bc
Braden Schaeffer <braden.schaeffer@gmail.com>
2015-01-30 14:30:00 -0500

    Rework choice response ratios
    - Ensures all ratios add up to 100% when there are responses to
      calculate
    - Ensures all ratios are 0% when there are not responses to calculate

---
+++ lib/choice_result_cache.rb
@@ -0,0 +1,52 @@
+class ChoiceResultCache < Struct.new(:question)
+
+  def response_ratio_for(choice)
+    choice_ratios[choice.id]
+  end
+
+  # Builds a result object for all choice responses at the same time
+  # so that child records can access this data through the parent but
+  # don't have to calculate it themselves.
+  #
+  def choice_ratios
+    @choice_ratios ||= begin
+      unless response_count > 0
+        # Any requested ratio would be 0.00 anyway
+        return Hash.new { |h, k| h[k] = 0.00 }
+      end
+
+      total = 0
+      remainder = 100
+      results = {}
+
+      data = question.choices.map do |choice|
+        percent = (choice.responses.size * 100) / response_count.to_f
+        whole = percent.to_i
+        total += whole
+        remainer -= whole
+        {id: choice.id, decimals: percent % 1, whole: whole}
+      end
+
+      if remainder > 0
+        data.sort! { |a, b| b[:decimals] <=> a[:decimals] }
+      end
+
+      data.each do |d|
+        id, value = d[:id], d[:whole]
+
+        if remainder > 0
+          value += 1
+          remainder -= 1
+        end
+
+        results[id] = (value.to_f / 100).round(2)
+      end
+
+      results
+    end
+  end
+
+  def response_count
+    @response_count ||= question.responses.size
+  end
+end
---
+++ spec/lib/choice_result_cache_spec.rb
@@ -0,0 +1,87 @@
+require 'rails_helper'
+require 'choice_result_cache'
+
+RSpec.describe ChoiceResultCache do
+
+  let(:question) { Question.new }
+  let(:cache) { ChoiceResultCache.new(question) }
+
+  describe '#response_ratio_for' do
+    it 'delegates to the choice_ratios hash' do
+      expect(cache).to receive(:choice_ratios).and_return(1 => :ratio)
+      choice = double('Choice', id: 1)
+      expect(cache.response_ratio_for(choice)).to eq(:ratio)
+    end
+  end
+
+  describe '#choice_ratios' do
+    let(:response_count) { 42 }
+    let(:test_choices) do
+      [
+        double('Choice', id: 1, responses: [*1..10]),
+        double('Choice', id: 2, responses: [*1..5]),
+        double('Choice', id: 3, responses: [*1..13]),
+        double('Choice', id: 4, responses: [*1..14])
+      ]
+    end
+
+    before do
+      allow(cache).to receive(:response_count) { response_count }
+      allow(question).to receive(:choices).and_return(test_choices)
+    end
+
+    context 'when all choices have responses' do
+      it 'calculates the correct ratios' do
+        expect(cache.choice_ratios[1]).to eq(0.24)
+        expect(cache.choice_ratios[2]).to eq(0.12)
+        expect(cache.choice_ratios[3]).to eq(0.31)
+        expect(cache.choice_ratios[4]).to eq(0.33)
+      end
+    end
+
+    context 'when some choices do not have responses' do
+      let(:response_count) { 12 }
+      let(:test_choices) do
+        [
+          double('Choice', id: 1, responses: [*1..7]),
+          double('Choice', id: 2, responses: [*1..5]),
+          double('Choice', id: 3, responses: [])
+        ]
+      end
+
+      it 'calculates the correct ratios' do
+        expect(cache.choice_ratios[1]).to eq(0.59)
+        expect(cache.choice_ratios[2]).to eq(0.41)
+        expect(cache.choice_ratios[3]).to eq(0.00)
+      end
+    end
+
+    context 'when all choices do not have responses' do
+      let(:response_count) { 0 }
+      let(:test_choices) do
+        [
+          double('Choice', id: 1, responses: []),
+          double('Choice', id: 2, responses: [])
+        ]
+      end
+
+      it 'calculates the correct ratios' do
+        expect(cache.choice_ratios[1]).to eq(0.00)
+        expect(cache.choice_ratios[2]).to eq(0.00)
+      end
+    end
+
+    it 'memoizes the results' do
+      expect(question).to receive(:choices).once
+      cache.choice_ratios
+      cache.choice_ratios
+    end
+  end
+
+  describe 'response_count' do
+    it 'delegates to the question attribute' do
+      expect(question).to receive(:responses) { [1, 2, 3] }
+      expect(cache.response_count).to eq(3)
+    end
+  end
+end
