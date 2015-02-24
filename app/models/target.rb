e7c6a03b1abfeee888378b5c2981e8a382bf12bc
Braden Schaeffer <braden.schaeffer@gmail.com>
2015-02-23 23:43:10 -0500

    Target enqueues question/feed processing

--- app/models/target.rb
+++ app/models/target.rb
@@ -15,48 +15,19 @@ class Target < ActiveRecord::Base
     !!all_users
   end
 
-  # Add the question to all of the appropriate users' feeds
-  # Will not add a question to a user who already has this question in their feed
+  # Sends the question to be added to feeds in the background while updating the
+  # `question#kind` attribute based on the targets attributes.
+  #
   def apply_to_question question
-    ActiveRecord::Base.transaction do
-      question.kind = all_users ? "public" : "targeted"
-      question.activate!
-
-      if all_followers
-        user.followers.each { |follower| target_user follower, question }
-      else
-        followers.each do |follower|
-          target_user follower, question if follower.leaders.include?(user)
-        end
-      end
-
-      if all_groups
-        user.group_members.each { |member| target_user member, question }
-      else
-        groups.each do |group|
-          group.member_users.each { |member| target_user member, question } if group.user == user
-        end
-      end
-    end
+    question.update_attribute(:kind, all_users ? "public" : "targeted")
+    Resque.enqueue(AddQuestionToAllFeeds, question.id)
   end
 
   private
 
-    def set_defaults
-      self.all_users = false if all_users.nil?
-      self.all_followers = false if all_followers.nil?
-      self.all_groups = false if all_groups.nil?
-    end
-
-    def target_user user, question
-      item = user.feed_items.find_by question_id:question
-
-      if item
-        item.update_attributes why: "targeted"
-      else
-        user.feed_items << FeedItem.new(question:question, relevance:1, why: "targeted")
-      end
-
-      question.add_and_push_message user
-    end
+  def set_defaults
+    self.all_users = false if all_users.nil?
+    self.all_followers = false if all_followers.nil?
+    self.all_groups = false if all_groups.nil?
+  end
 end
