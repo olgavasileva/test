6c9e296183180edad0cb490a791625fa0318e558
Troy Anderson (laptop) <troy@tlainvestments.com>
2015-03-09 13:34:09 -0700

    Added type index to users table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150306035930) do
+ActiveRecord::Schema.define(version: 20150309203137) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -994,6 +994,7 @@ ActiveRecord::Schema.define(version: 20150306035930) do
 
   add_index "users", ["email"], name: "index_users_on_email", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["type"], name: "index_users_on_type", using: :btree
   add_index "users", ["user_avatar_id"], name: "index_users_on_user_avatar_id", using: :btree
 
   create_table "users_roles", id: false, force: true do |t|
