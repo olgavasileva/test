421a0f51543433161c334534cb07d0f4267efa86
Braden Schaeffer <braden.schaeffer@gmail.com>
2015-03-26 15:45:06 -0400

    Cleanup MultipleChoiceResponse spec

--- spec/models/multiple_choice_response_spec.rb
+++ spec/models/multiple_choice_response_spec.rb
@@ -1,29 +1,16 @@
 require 'rails_helper'
 
 describe MultipleChoiceResponse do
-  subject { FactoryGirl.create :multiple_choice_response }
+  it { is_expected.to have_many(:choices_responses) }
+  it { is_expected.to have_many(:choices).through(:choices_responses) }
 
-  describe :validations do
-    it {expect(FactoryGirl.build(:multiple_choice_response)).to be_valid}
-  end
-
-  describe :relations do
-    let(:multiple_choice_response) {FactoryGirl.create :multiple_choice_response}
-
-    it {multiple_choice_response.should be_valid}
-    it {multiple_choice_response.question.should be_present}
-    it {multiple_choice_response.question.class.should eq MultipleChoiceQuestion}
-    it {multiple_choice_response.choices_responses.build.class.should eq ChoicesResponse}
-  end
-
-  describe :text do
+  describe '#text' do
     before do
-      subject.choices = FactoryGirl.create_list(:multiple_choice, 3,
-                                                title: "hello")
+      subject.choices = FactoryGirl.build_list(:multiple_choice, 2)
     end
 
     it "returns combined text of choices" do
-      expect(subject.text).to eq "hello, hello, hello"
+      expect(subject.text).to eq(subject.choices.map(&:title).join(', '))
     end
   end
 end
