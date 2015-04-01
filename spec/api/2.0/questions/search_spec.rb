5eed17a5fe11caf160bfc1a191fdb9dba7893e41
Braden Schaeffer <braden.schaeffer@gmail.com>
2015-04-01 18:08:22 -0400

    Extend Question.search_for to include tags

--- spec/api/2.0/questions/search_spec.rb
+++ spec/api/2.0/questions/search_spec.rb
@@ -17,19 +17,19 @@ describe :search do
       let(:user) {instance.user}
 
       context "With some questions" do
-        let(:q1) { FactoryGirl.create :text_choice_question, title: "Would you like to dance?" }
-        let(:c11) { FactoryGirl.create :text_choice, question: q1, title: "Text Choice 11", rotate: true }
-        let(:c12) { FactoryGirl.create :text_choice, question: q1, title: "Text Choice 12", rotate: true }
-        let(:c13) { FactoryGirl.create :text_choice, question: q1, title: "Text Choice 13", rotate: false }
-        let(:q2) { FactoryGirl.create :text_choice_question, title: "Green Eggs or Ham?" }
-        let(:c21) { FactoryGirl.create :text_choice, question: q2, title: "Text Choice 21", rotate: true }
-        let(:q3) { FactoryGirl.create :text_question, title: "How much wood would a wood chuck chuck if a wood chuck could chuck wood?" }
+        let!(:q1) { FactoryGirl.create :text_choice_question, title: "Would you like to dance?" }
+        let!(:c11) { FactoryGirl.create :text_choice, question: q1, title: "Text Choice 11", rotate: true }
+        let!(:c12) { FactoryGirl.create :text_choice, question: q1, title: "Text Choice 12", rotate: true }
+        let!(:c13) { FactoryGirl.create :text_choice, question: q1, title: "Text Choice 13", rotate: false }
+        let!(:q2) { FactoryGirl.create :text_choice_question, title: "Green Eggs or Ham?" }
+        let!(:c21) { FactoryGirl.create :text_choice, question: q2, title: "Text Choice 21", rotate: true }
+        let!(:q3) { FactoryGirl.create :text_question, title: "How much wood would a wood chuck chuck if a wood chuck could chuck wood?" }
+        let!(:q4) { FactoryGirl.create :text_question, tag_list: ['eggs'] }
 
-        let(:all_questions) { [q1, q2, q3] }
+        let(:all_questions) { [q1, q2, q3, q4] }
 
         let(:setup_questions) {
           all_questions
-
           c11
           c12
           c13
@@ -39,30 +39,37 @@ describe :search do
         context "When the search string doesn't match any questions" do
           let(:search_text) { "NO MATCH" }
 
-          it { expect(response.status).to eq 201 }
-          it { expect(json).to eq [] }
+          it 'returns the correct result' do
+            expect(response.status).to eq 201
+            expect(json).to eq []
+          end
         end
 
-        context "When the search string matches one question" do
-          let(:search_text) { "Green Eggs" }
+        context "When the search string matches one question and one tag" do
+          let(:search_text) { "Eggs" }
 
-          it { expect(response.status).to eq 201 }
-          it { expect(json.map { |q| q["question"]["id"] }).to eq [q2.id] }
-          it { expect(json[0]["question"].keys).to match_array ["id", "anonymous", "category", "choices", "comment_count", "created_at", "creator_id", "creator_name", "description", "image_url", "member_community_ids", "response_count", "rotate", "title", "type", "uuid", "user_answered"] }
+          it 'returns the correct result' do
+            expect(response.status).to eq 201
+            expect(json.map { |q| q["question"]["id"] }).to eq [q4.id, q2.id]
+          end
         end
 
         context "When the search string matches two questions" do
           let(:search_text) { "would" }
 
-          it { expect(response.status).to eq 201 }
-          it { expect(json.map { |q| q["question"]["id"] }).to eq [q3.id, q1.id] }
+          it 'returns the correct result' do
+            expect(response.status).to eq 201
+            expect(json.map { |q| q["question"]["id"] }).to eq [q3.id, q1.id]
+          end
         end
 
         context "When the search string matches the choice of one of the questions" do
           let(:search_text) { "Choice 21" }
 
-          it { expect(response.status).to eq 201 }
-          it { expect(json.map { |q| q["question"]["id"] }).to eq [q2.id] }
+          it 'returns the correct result' do
+            expect(response.status).to eq 201
+            expect(json.map { |q| q["question"]["id"] }).to eq [q2.id]
+          end
         end
 
         context 'include_skipped = true' do
