a5b71026acc3912a28a006768ed0649bb4085cf8
Troy Anderson <troy@erst.us>
2015-04-22 22:00:56 -0700

    Support servers without anything scheduled in the resque scheduler.

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -6,4 +6,5 @@ redis_url = ENV["REDISCLOUD_URL"] || "localhost:6379:resque/resque:#{Rails.env}"
 Resque.redis = redis_url
 
 # configure the schedule
-Resque.schedule = YAML.load_file(Rails.root.join('config', 'schedule.yml'))
+resque_config = YAML.load_file(Rails.root.join('config', 'schedule.yml'))
+Resque.schedule = resque_config if resque_config  # support servers without anything scheduled
