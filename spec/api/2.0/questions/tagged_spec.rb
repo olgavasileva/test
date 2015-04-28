1f1fcb612bd7e09ea9045d4fed3b73b34c7252f9
Troy Anderson <troy@erst.us>
2015-04-28 09:03:49 -0700

    Added spec to test any: true on tagged_with.

--- spec/api/2.0/questions/tagged_spec.rb
+++ spec/api/2.0/questions/tagged_spec.rb
@@ -39,5 +39,21 @@ RSpec.describe TwoCents::Questions do
       get 'v/2.0/questions/tagged', auth_token: token, tag: 'other'
       expect(json.length).to eq(0)
     end
+
+    context "When there are 2 questions each with the same 2 tags and a question with only one of the tags" do
+      let(:tag1) {"tag1"}
+      let(:tag2) {"tag2"}
+      let(:tags) {[tag1, tag2]}
+      let(:question1) {FactoryGirl.create :text_question, tag_list:[tag1, tag2]}
+      let(:question2) {FactoryGirl.create :text_question, tag_list:[tag1, tag2]}
+      let(:question3) {FactoryGirl.create :text_question, tag_list:[tag1]}
+      before {@questions = [question1, question2, question3]}
+
+      it { expect(Question.tagged_with(tag1)).to match_array [question1, question2, question3] }
+      it { expect(Question.tagged_with(tag2)).to match_array [question1, question2] }
+      it { expect(Question.tagged_with(tags)).to match_array [question1, question2] }
+      it { expect(Question.tagged_with(tags, any: false)).not_to match_array [question1, question2, question3] }
+      it { expect(Question.tagged_with(tags, any: true)).to match_array [question1, question2, question3] }
+    end
   end
 end
