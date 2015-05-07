b46283b51d5cb96b3d64d5b287cf86041e0bfc1e
Braden Schaeffer <braden.schaeffer@gmail.com>
2015-05-07 14:43:28 -0400

    Add Setting.fetch_values to enable single queries

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -3,6 +3,11 @@ class Setting < ActiveRecord::Base
 
   scope :enabled, -> { where enabled: true }
 
+  def self.fetch_values(*keys)
+    settings = Hash[Setting.where(key: keys).pluck(:key, :value)]
+    settings.with_indifferent_access
+  end
+
   def self.fetch_value(key, default=nil)
     Setting.find_by(key: key).try(:value) || default
   end
