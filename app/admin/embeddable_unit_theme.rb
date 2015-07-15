0af8ac6880410e3162b7dfc5784bf0f3121bc7c8
sergey-kintsel <serjoya@mail.ru>
2015-07-15 16:48:37 +0600

    fixed active admin

--- app/admin/embeddable_unit_theme.rb
+++ app/admin/embeddable_unit_theme.rb
@@ -3,7 +3,7 @@ ActiveAdmin.register EmbeddableUnitTheme do
 
   permit_params :title, :main_color, :user_id, :color1, :color2
 
-  filter :user
+  filter :user_username, as: :string, label: 'Username'
   filter :title
   filter :main_color
   filter :color1
@@ -21,5 +21,16 @@ ActiveAdmin.register EmbeddableUnitTheme do
     column :color1
     column :color2
     actions
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :user_id, as: :number
+      f.input :title
+      f.input :main_color
+      f.input :color1
+      f.input :color2
+    end
+    f.actions
   end
 end
