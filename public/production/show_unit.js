0294b8203340097a1e1da5f9a2fb98f938fb5b00
sergey-kintsel <serjoya@mail.ru>
2015-07-22 22:01:16 +0600

    added scrolling=no for iframes with embeds

--- public/production/show_unit.js
+++ public/production/show_unit.js
@@ -1,6 +1,7 @@
 var iframe = document.createElement('iframe');
 iframe.width = statisfy_unit_width;
 iframe.height = statisfy_unit_height;
+iframe.scrolling = 'no';
 iframe.src = "http://a.statisfy.co/unit/" + statisfy_unit;
 iframe.frameBorder = 0;
 document.body.appendChild(iframe);
--- public/staging/show_unit.js
+++ public/staging/show_unit.js
@@ -1,6 +1,7 @@
 var iframe = document.createElement('iframe');
 iframe.width = statisfy_unit_width;
 iframe.height = statisfy_unit_height;
+iframe.scrolling = 'no';
 iframe.src = "http://staging.statisfy.co/unit/" + statisfy_unit;
 iframe.frameBorder = 0;
 document.body.appendChild(iframe);
