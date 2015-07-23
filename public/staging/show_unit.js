e59801a224c81846febacad2256c073f217858ff
sergey-kintsel <serjoya@mail.ru>
2015-07-23 09:47:00 +0600

    fixed JS embed

--- public/staging/show_unit.js
+++ public/staging/show_unit.js
@@ -2,6 +2,6 @@ var iframe = document.createElement('iframe');
 iframe.width = statisfy_unit_width;
 iframe.height = statisfy_unit_height;
 iframe.scrolling = 'no';
-iframe.src = "http://staging.statisfy.co/unit/" + statisfy_unit;
+iframe.src = "http://staging.statisfy.co/qp/" + statisfy_unit + '/' + (statisfy_unit_type || 'medium_rectangle');
 iframe.frameBorder = 0;
 document.body.appendChild(iframe);
