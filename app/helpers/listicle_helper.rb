5a75c77d7143fbf3a85bdc11bee31e749d09061b
sergey-kintsel <serjoya@mail.ru>
2015-10-01 21:17:33 +0600

    hotfix for listicles UI

--- app/helpers/listicle_helper.rb
+++ app/helpers/listicle_helper.rb
@@ -3,4 +3,16 @@ module ListicleHelper
   def text_from_html(html)
     Nokogiri.HTML(html).text
   end
+
+  def limit_text(text, length = 30)
+    if length >= text.length
+      text
+    else
+      text[0..length-4] + '...'
+    end
+  end
+
+  def text_or_empty_symbol(text)
+    text.nil? || text.empty? ? '&nbsp;' : text
+  end
 end
