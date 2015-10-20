66f81d31482f1583739e9b974dc89f4ebe186170
Sergey Kintsel <serjonya@gmail.com>
2015-10-20 12:44:01 +0600

    fixed auth_token

--- app/models/respondent.rb
+++ app/models/respondent.rb
@@ -237,8 +237,7 @@ class Respondent < ActiveRecord::Base
   def web_app_url_with_auth(path='')
     instance = instances.last
     instance = Instance.create_null_instance(self) if instance.nil?
-    instance.auth_token
-    instance.uuid
+    instance.refresh_auth_token
     "#{ENV['WEB_APP_URL']}/#/#{path}?auth_token=#{instance.auth_token}" +
         "&instance_token=#{instance.uuid}&registered_user=#{username}"
   end
